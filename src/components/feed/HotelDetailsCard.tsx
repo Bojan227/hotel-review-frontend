501fb3671cd6a0ae77a77bad694ad8fb8f53be2d
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-26 19:23:24 +0100

    Add Hotel Details Card component

---
+++ src/components/feed/HotelDetailsCard.tsx
@@ -0,0 +1,28 @@
+import { HotelDataType } from './types/types';
+
+export default function HotelDetailsCard({
+  _id,
+  imageUrl,
+  hotelName,
+  address,
+  text,
+  imageId,
+}: HotelDataType) {
+  return (
+    <div className="hotel-details-card">
+      <img src={imageUrl} />
+      <div className="hotel-details-card-info">
+        <div>
+          <h2>{hotelName}</h2>
+          <p>{address}</p>
+        </div>
+
+        <h4>Overall Rating</h4>
+        <div>
+          <h1>Description</h1>
+          <p>{text}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
