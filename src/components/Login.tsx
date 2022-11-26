738dc47c7cde8a90b1d5d5cae45c39a5c3be6604
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-26 13:14:37 +0100

    Add Login component and LoginButton

---
+++ src/components/Login.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import InputField from './InputField';
+import useLogin from '../hooks/useLogin';
+import { LoginButton } from './buttons/LoginButton';
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { login, error } = useLogin();
+
+  const handleSubmit = async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    await login(email, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email
+        <InputField
+          type="text"
+          value={email}
+          onChange={(email) => setEmail(email)}
+        />
+      </label>
+      <label htmlFor="password">
+        Password
+        <InputField
+          type="password"
+          value={password}
+          onChange={(password) => setPassword(password)}
+        />
+      </label>
+      <LoginButton
+        className="login-btn"
+        disabled={!email || !password ? true : false}
+      >
+        Login
+      </LoginButton>
+    </form>
+  );
+}
---
+++ src/components/buttons/LoginButton.tsx
@@ -0,0 +1,18 @@
+import React, { MouseEvent } from 'react';
+import Button from './Button';
+
+export const LoginButton = ({
+  children,
+  disabled,
+  className,
+}: {
+  children: React.ReactNode;
+  disabled: boolean;
+  className: string;
+}) => {
+  return (
+    <Button disabled={disabled} className={className}>
+      {children}
+    </Button>
+  );
+};
