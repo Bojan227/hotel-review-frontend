c31538d68d29764bd545ae883b5a6ea34c8434f6
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-26 13:08:01 +0100

    Add custom useLogin hook

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,37 @@
+import useUserContext from './useUserContext';
+
+import { useState } from 'react';
+
+export default function useLogin() {
+  const userContext = useUserContext();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const login = async (email: string, password: string) => {
+    setIsLoading(true);
+    const res = await fetch('http://localhost:3000/user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    const { user, token, error } = await res.json();
+
+    if (res.ok) {
+      userContext?.dispatch({ type: 'LOGIN', payload: user });
+      localStorage.setItem('user', JSON.stringify(user));
+      document.cookie = 'token' + '=' + (token || '') + '; path=/';
+      setIsLoading(false);
+    } else {
+      setError(error);
+      setIsLoading(false);
+    }
+  };
+
+  return { login, error, isLoading };
+}
