d694bb40db54d1439156089599590b2a6c4d1034
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-27 16:29:37 +0100

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import './App.css';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Home from './pages/Home';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import useUserContext from './hooks/useUserContext';
+import Navigation from './components/navigation/NavigationContainer';
+import HotelDetails from './components/feed/HotelDetails';
 
 function App() {
+  const userContext = useUserContext();
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={userContext?.user ? <Home /> : <Signup />} />
+          <Route
+            path="login"
+            element={userContext?.user ? <Navigate to="/" /> : <Login />}
+          />
+          <Route path="h/:hotelId" element={<HotelDetails />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
