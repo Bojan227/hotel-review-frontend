db4f41c48dc1494f4e2be15d52a5f67831755d43
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-27 12:40:17 +0100

    Add Create new review functionality

--- src/components/reviews/CreateReviewForm.tsx
+++ src/components/reviews/CreateReviewForm.tsx
@@ -1,18 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, Dispatch, SetStateAction } from 'react';
+import { useParams } from 'react-router-dom';
 import useCreateReview from '../../hooks/useCreateReview';
+import { ReviewButton } from '../buttons/ReviewButton';
 import { StarRating } from '../StarRating';
+import { ReviewType } from './types/ReviewTypes';
 
-export default function CreateReviewForm() {
+export default function CreateReviewForm({
+  setReviews,
+}: {
+  setReviews: Dispatch<SetStateAction<ReviewType[] | undefined>>;
+}) {
   const [text, setText] = useState('');
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
-
+  const { hotelId } = useParams();
   const { isLoading, error, createReview } = useCreateReview();
 
   async function handleSubmit(e: React.SyntheticEvent) {
     e.preventDefault();
+    await createReview(text, hotelId, rating, setReviews);
+    setText(() => '');
+    setHover(() => 0);
   }
 
+  console.log(rating);
   return (
     <form onSubmit={handleSubmit} className="review-form">
       <textarea
@@ -21,7 +32,12 @@ export default function CreateReviewForm() {
         placeholder="Tell us your experience"
       />
       <StarRating {...{ rating, setRating, hover, setHover }} />
-      <button>Rate now</button>
+      <ReviewButton
+        className="review-btn"
+        disabled={rating === 0 || !text ? true : false}
+      >
+        Rate
+      </ReviewButton>
     </form>
   );
 }
