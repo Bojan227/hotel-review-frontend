e7e858e6f653736cc4006cde0b7450faad7aeefb
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-28 12:22:48 +0100

    Add custom hooks for updating favourites hotels

---
+++ src/hooks/useUpdateFavourites.ts
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import useUserContext from './useUserContext';
+
+export default function useUpdateFavourites() {
+  const userContext = useUserContext();
+  const [error, setError] = useState('');
+
+  const updateFavourites = async ({ hotelId }: { hotelId: string }) => {
+    try {
+      const res = await fetch('http://localhost:3000/user/favourites', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${document.cookie.split('=')[1]}`,
+        },
+        body: JSON.stringify({
+          hotelId,
+        }),
+      });
+      const { user, hotel } = await res.json();
+      userContext.dispatch({ type: 'UPDATE_FAV', payload: hotel });
+      localStorage.setItem('user', JSON.stringify(user));
+    } catch (error) {
+      setError('Cannot find any hotel');
+    }
+  };
+
+  return { updateFavourites };
+}
