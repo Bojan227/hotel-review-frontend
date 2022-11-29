7c7de3a45bf741f502e9f9e98193d972881a29c2
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-29 15:32:06 +0100

    Set page as query param and render load button on condition

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,43 @@
 import { useEffect, useState } from 'react';
 import HotelContainer from '../components/feed/HotelContainer';
-import useFetch from '../hooks/useFetch';
 import SearchContainer from '../components/SearchContainer';
+import useGetHotels from '../hooks/useGetHotels';
 
 export default function Home() {
   const [name, setName] = useState('');
   const [address, setAddress] = useState('');
-
-  const { getData, data } = useFetch();
+  const [page, setPage] = useState(0);
+  const { getHotels, hotels, setHotels, numberOfHotels } = useGetHotels();
   const [isLoading, setIsLoading] = useState(false);
+
+  console.log(page);
 
   useEffect(() => {
     setIsLoading(true);
+
     const timeout = setTimeout(async () => {
-      getData(`http://localhost:3000/hotel/?name=${name}&address=${address}`);
+      getHotels(
+        `http://localhost:3000/hotel/?name=${name}&address=${address}&page=${page}`
+      );
       setIsLoading(false);
     }, 2000);
 
     return () => clearTimeout(timeout);
-  }, [name, address]);
-
+  }, [name, address, page]);
   return (
     <main>
-      <SearchContainer {...{ name, setName, address, setAddress }} />
+      <SearchContainer
+        {...{ name, setName, address, setAddress, setPage, setHotels }}
+      />
       {isLoading && <h1>Loading...</h1>}
-      {data && <HotelContainer hotelData={data} />}
+      {hotels && <HotelContainer hotelData={hotels} />}
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : numberOfHotels && hotels.length < numberOfHotels ? (
+        <button onClick={() => setPage((prevPage) => prevPage + 1)}>
+          Load more
+        </button>
+      ) : null}
     </main>
   );
 }
