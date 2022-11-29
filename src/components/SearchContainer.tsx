b93b8f1fc6fbbaa45de2d01b4af548e632d171cb
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-29 15:23:12 +0100

    Add set hotels and setPage state setters

--- src/components/SearchContainer.tsx
+++ src/components/SearchContainer.tsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from 'react';
+import { HotelDataType } from './feed/types/types';
 import InputField from './InputField';
 export default function SearchContainer({
   name,
   address,
   setAddress,
   setName,
+  setHotels,
+  setPage,
 }: {
   name: string;
   address: string;
   setAddress: React.Dispatch<React.SetStateAction<string>>;
   setName: React.Dispatch<React.SetStateAction<string>>;
+  setHotels: React.Dispatch<React.SetStateAction<HotelDataType[]>>;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
 }) {
   return (
     <form>
@@ -17,13 +22,21 @@ export default function SearchContainer({
       <InputField
         type="search"
         value={name}
-        onChange={(name) => setName(name)}
+        onChange={(name) => {
+          setName(name);
+          setHotels([]);
+          setPage(0);
+        }}
       />
       Search By Address
       <InputField
         type="search"
         value={address}
-        onChange={(address) => setAddress(address)}
+        onChange={(address) => {
+          setAddress(address);
+          setHotels([]);
+          setPage(0);
+        }}
       />
     </form>
   );
